b2edcb786b2b9ef8212240ebad9395921280705b
Robert Fernandes <robertkirka97@gmail.com>
2025-02-13 15:11:35 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import com.educandoweb.course.entities.pk.OrderItemPk;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -16,10 +17,11 @@ public class OrderItem  implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
-	private	OrderItemPk id;
+	private	OrderItemPk id = new OrderItemPk();
 	
 	private Integer quantity;
 	private Double price;
+	
 	
 	public OrderItem () {
 		
@@ -32,7 +34,8 @@ public class OrderItem  implements Serializable{
 		this.quantity = quantity;
 		this.price = price;
 	}
-
+	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPk.java
@@ -6,11 +6,9 @@ import java.util.Objects;
 import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.Product;
 
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
-@Embeddable
 public class OrderItemPk implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
